032da2cae653e40fed54ad54c2bc7a1e62dffa40
john-emerson14 <john.carv.sousa@gmail.com>
2020-06-18 16:43:39 -0300

    feat: add connection to mongo database

--- src/app.js
+++ src/app.js
@@ -1,13 +1,22 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 class App {
     constructor() {
         this.app = express()
 
+        this.database()
         this.routes()
 
         this.app.listen(3000, function() {
             console.log("Servidor rodando na porta 3000")
+        })
+    }
+
+    database() {
+        mongoose.connect('URI', { useNewUrlParser: true, useUnifiedTopology: true })
+        mongoose.connection.on('error', function() {
+            console.log('Erro ao conectar-se ao BD')
         })
     }
 
